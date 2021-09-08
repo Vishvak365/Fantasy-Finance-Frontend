3341e025c82be78f6c84ad17bbc89ea7c49ab6ee
munisht06 <munishtanwar77@gmail.com>
2021-09-08 12:58:51 -0400

    welcome msg & checkout option added to onboard pg

--- src/pages/UserOnBoard.js
+++ src/pages/UserOnBoard.js
@@ -1,6 +1,25 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
-function UserOnBoard() {
-  return <div>This is the user onboard page</div>;
+function UserOnBoard(props) {
+  let history = useHistory();
+  return (
+    <div>
+      <h1>Welcome to OnlyFinance</h1>
+      <body>
+        <p>OnlyFinance allows your to join fantasy league where you can buy and sell  stocks</p>
+        <form
+          action={`https://fantasy-finance-backend.herokuapp.com/checkout?token=${props.token}`}
+          method="POST"
+        >
+          You are can join the preimum account for $12
+          <button type="submit" role="link">
+            Join Premium
+          </button>
+        </form>
+        <button onClick={() => history.push("/")}>Start</button>
+      </body>
+    </div>
+  );
 }
-export default UserOnBoard
+export default UserOnBoard;
