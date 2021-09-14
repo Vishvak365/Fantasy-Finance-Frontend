c73eb671ebd1c668934327b75d239832b8fb5087
munisht06 <munishtanwar77@gmail.com>
2021-09-14 12:18:11 -0400

    Sending new user data to back end

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,21 +2,51 @@ import React from "react";
 import googleSignin from "../images/google_signin.png";
 import firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/firestore";
 import { useHistory } from "react-router-dom";
+import client from "../util/Client";
 
-const Login = () => {
-
+const Login = (props) => {
   let history = useHistory();
+  // ,(req, res) => {
+  //   const body = req.body;
+
+  //   try {
+  //     users
+  //       .doc()
+  //       .set({
+  //         ...body,
+  //         name: user.user.displayName,
+  //         Premium: false,
+  //         created: firebase.firestore.Timestamp.now(),
+  //       })
+  //       .then((data) => {
+  //         res.status(200);
+  //         res.json(data);
+
+  //         return;
+  //       });
+  //   } catch (exception) {
+  //     console.log(exception);
+  //     res.status(500);
+  //     res.send({ message: "error in creating user" });
+  //   }
+  // });
+
+  // const addNewUser = (user) => {
+  //   client.post("/users/createUser")
+  // };
 
   function googleSignInPopup() {
-  
     const provider = new firebase.auth.GoogleAuthProvider();
     firebase
       .auth()
       .signInWithPopup(provider)
       .then((user) => {
+        console.log(props.token)
         if (user.additionalUserInfo.isNewUser === true) {
           history.push("/onboard");
+          client.post("users/createUser");
         }
       })
       .catch((error) => {
