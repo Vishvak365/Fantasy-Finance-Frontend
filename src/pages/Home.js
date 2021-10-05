862f5ea0c7af1d6cafe76604f9147736069553ea
sophielesher <36457307+sophielesher@users.noreply.github.com>
2021-10-04 21:26:04 -0400

    original home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,6 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import client from "../util/Client";
 import Paper from "@material-ui/core/Paper";
-import {blueGrey, deepOrange, deepPurple} from "@material-ui/core/colors";
 var faker = require("faker");
 export default function Home(props) {
   const [history, setHistory] = React.useState([]);
@@ -20,7 +19,6 @@ export default function Home(props) {
     width: "80%",
     margin: "auto",
     marginBottom: 10,
-      //background: deepPurple,
   };
   return (
     <div>
@@ -28,9 +26,7 @@ export default function Home(props) {
         action={`https://fantasy-finance-backend.herokuapp.com/checkout?token=${props.token}`}
         method="POST"
       >
-        <button
-            className={"button_5"}
-            type="submit" role="link">
+        <button type="submit" role="link">
           Checkout
         </button>
       </form>
@@ -38,32 +34,14 @@ export default function Home(props) {
       <img src={firebase.auth().currentUser.photoURL} alt="User Profile" />
       <h1>{firebase.auth().currentUser.displayName}</h1>
       <button
-          className={"button_3"}
-          onClick={() => {
+        onClick={() => {
           firebase.auth().signOut();
         }}
       >
         Sign Out
       </button>
-        <button
-            className={"button_2"}
-            onClick={() => {
-                firebase.auth().signOut();
-            }}
-        >
-            Buy
-        </button>
-        <button
-            className={"button_1"}
-            onClick={() => {
-                firebase.auth().signOut();
-            }}
-        >
-            Sell
-        </button>
       <button
-          className={"button_4"}
-          onClick={() => {
+        onClick={() => {
           client.get(
             `/trade/makeTrade?stockName=${faker.finance.currencyCode()}&action=${
               ["BUY", "SELL"][Math.floor(Math.random() * 2)]
@@ -73,7 +51,7 @@ export default function Home(props) {
       >
         Create Random Transaction
       </button>
-      <div >
+      <div>
         {history.length !== 0
           ? history.map((row) => {
               return (
