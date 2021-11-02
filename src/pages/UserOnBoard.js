dff8027302fdd6c26850e1f11b490f00aa1076a3
Ashish Goolla <ashish.goolla@gmail.com>
2021-11-02 15:29:05 -0400

    User Onboarding name change to Fantasy Finance

--- src/pages/UserOnBoard.js
+++ src/pages/UserOnBoard.js
@@ -1,13 +1,15 @@
 import React from "react";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper"; 
 import { useHistory } from "react-router-dom";
 
 function UserOnBoard(props) {
   let history = useHistory();
   return (
     <div>
-      <h1>Welcome to OnlyFinance</h1>
+      <h1>Welcome to Fantasy Finance</h1>
       <body>
-        <p>OnlyFinance allows your to join fantasy league where you can buy and sell  stocks</p>
+        <p>Fantasy Finance allows your to join fantasy league where you can buy and sell stocks</p>
         <form
           action={`https://fantasy-finance-backend.herokuapp.com/checkout?token=${props.token}`}
           method="POST"
