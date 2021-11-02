4e179ef80f37ecb00dd7885c94fef5f8a9d91810
munisht06 <munishtanwar77@gmail.com>
2021-11-02 15:04:14 -0400

    loginPage done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,19 +3,30 @@ import googleSignin from "../images/google_signin.png";
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
+import googleLogo from "../images/googleLogo.png";
+import {
+  Avatar,
+  Grid,
+  Paper,
+  Button,
+  Typography,
+  Container,
+} from "@material-ui/core";
 import { useHistory } from "react-router-dom";
 import client from "../util/Client";
 
+
+
 const Login = (props) => {
   let history = useHistory();
-    
+
   function googleSignInPopup() {
     const provider = new firebase.auth.GoogleAuthProvider();
     firebase
       .auth()
       .signInWithPopup(provider)
       .then((user) => {
-        console.log(props.token)
+        console.log(props.token);
         if (user.additionalUserInfo.isNewUser === true) {
           history.push("/onboard");
           client.post("users/createUser");
@@ -26,17 +37,57 @@ const Login = (props) => {
       });
   }
 
+  const googleStyle = {
+    borderRadius: "25px",
+    textTransform: "none",
+    marginTop: 70,
+    display: "flex",
+    alignItems: "center",
+    boxShadow: 3,
+    backgroundColor: "#3f51b5",
+    color: "white",
+    transition: "background-color 0.5s",
+    "&:hover": {
+      backgroundColor: "#42a5f5",
+      transition: "background-color 0.5s",
+      cursor: "pointer",
+    },
+  };
+  const loginStyle = {
+    borderRadius: "25px",
+    padding: 20,
+    height: "40vh",
+    width: 380,
+    margin: "90px auto",
+  };
+
+
   return (
     <div>
-      <img
-        src={googleSignin}
-        style={{ cursor: "pointer" }}
-        onClick={() => {
-          googleSignInPopup();
-        }}
-        className="gsignin"
-        alt="Google Sign In Button"
-      />
+      <Grid>
+        <Paper elevation={10} style={loginStyle}>
+          <h1>Fantasy Finance</h1>
+          <Grid align="center">
+            <h2>Sign in</h2>
+            <Button
+              className="gsignin"
+              onClick={googleSignInPopup}
+              variant="contained"
+              style={googleStyle}
+            >
+              <Avatar src={googleLogo} />
+              <Typography
+                component="p"
+                variant="h6"
+                flexGrow="1"
+                textAlign="center"
+              >
+                Sign in with Google
+              </Typography>
+            </Button>
+          </Grid>
+        </Paper>
+      </Grid>
     </div>
   );
 };
