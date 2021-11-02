3e40fb5933ba74385cfef8535975f210462527bd
Vishvak Seenichamy <vishvak365@gmail.com>
2021-11-02 14:01:11 -0400

    setup base leagues explorer with router history and routing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,22 @@ import React from "react";
 import firebase from "firebase/app";
 import "firebase/auth";
 import client from "../util/Client";
-import Paper from '@material-ui/core/Paper';
-var faker = require("faker");
-export default function Home() {
+import LeaguesManage from "./home/LeaguesManage";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import LeagueHistory from "./home/LeagueHistory";
+export default function Home(props) {
   const [history, setHistory] = React.useState([]);
+  const [sampleTest, setSampleTest] = React.useState("");
+
   React.useEffect(() => {
+    console.log(props);
     async function fetchData() {
       const data = await client.get("/trade/history");
       setHistory(data.data);
+      const sampleData = await client.get("/sample/test");
+      setSampleTest(sampleData.data);
     }
     fetchData();
   }, []);
@@ -17,42 +25,40 @@ export default function Home() {
   const paperStyle = {
     padding: 10,
     width: "80%",
-    margin:'auto',
-    marginBottom:10
-  }
+    margin: "auto",
+    marginBottom: 10,
+  };
   return (
     <div>
+      <form
+        action={`https://fantasy-finance-backend.herokuapp.com/checkout?token=${props.token}`}
+        method="POST"
+      >
+        <button type="submit" role="link">
+          Purchase Premium
+        </button>
+        <button
+          onClick={() => {
+            firebase.auth().signOut();
+          }}
+        >
+          Sign Out
+        </button>
+      </form>
       <img src={firebase.auth().currentUser.photoURL} alt="User Profile" />
       <h1>{firebase.auth().currentUser.displayName}</h1>
-      <button
-        onClick={() => {
-          firebase.auth().signOut();
-        }}
-      >
-        Sign Out
-      </button>
-      <button
-        onClick={() => {
-          client.get(
-            `/trade/makeTrade?stockName=${faker.finance.currencyCode()}&action=${["BUY", "SELL"][Math.floor(Math.random() * 2)]
-            }`
-          );
-        }}
-      >
-        Create Random Transaction
-      </button>
-      <div>
-        {history.length !== 0
-          ? history.map((row) => {
-            return (
-              <Paper style={paperStyle} elevation={3}>
-                <p>
-                  {row.stock} - {row.action} - {row.executed._seconds}
-                </p>
-              </Paper>
-            );
-          })
-          : null}
+      <div style={{ width: "100%" }}>
+        <Box
+          sx={{
+            display: "grid",
+            gridTemplateColumns: "repeat(2, 2fr)",
+            p: 5,
+            gap: 4,
+          }}
+        >
+          <LeagueHistory />
+          <LeaguesManage />
+        </Box>
       </div>
     </div>
   );
---
+++ src/pages/home/LeagueHistory.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { styled } from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.primary,
+}));
+
+export default function LeagueHistory() {
+  return (
+    <Paper>
+      <h3>History</h3>
+    </Paper>
+  );
+}
