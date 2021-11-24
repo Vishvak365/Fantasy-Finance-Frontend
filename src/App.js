afb4d1165949490b64133d75a6722ead18dc7d01
Vishvak Seenichamy <vishvak365@gmail.com>
2021-11-23 20:55:10 -0500

    Add sidebar and add menubar

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { Router, Switch, Route } from "react-router-dom";
 import history from "./history";
 import LeagueTrade from "./pages/LeagueTrade";
 
+import Grid from "@mui/material/Grid";
 function App() {
   const [user, loading, error] = useAuthState(firebase.auth());
   const [token, setToken] = React.useState();
@@ -30,18 +31,62 @@ function App() {
       return (
         <div className="App">
           <Router history={history}>
-            <Switch>
-              <Route exact path="/">
-                <Home token={token} />
-              </Route>
-              <Route exact path="/onboard" component={UserOnBoard} />
-              <Route exact path="/league/:leagueID" component={League} />
-              <Route
-                exact
-                path="/league/:leagueID/trade/:stock"
-                component={LeagueTrade}
-              />
-            </Switch>
+            <Grid container>
+              <Grid item xs={2}>
+                <div
+                  style={{
+                    // backgroundColor: "#5866d3",
+                    height: "100vh",
+                    boxShadow: "24px 7px 46px -25px rgba(0,0,0,0.13)",
+                  }}
+                >
+                  <img
+                    alt="logo"
+                    style={{
+                      width: "100%",
+                      marginTop: "10%",
+                      textAlign: "center",
+                    }}
+                    src="https://travelwithgrant.boardingarea.com/wp-content/uploads/2017/04/robinhood-logo-green-800x150-610x114.png"
+                  />
+                  <div style={{ backgroundColor: "blue", height: 50 }}>
+                    adsf
+                  </div>
+                </div>
+              </Grid>
+              <Grid item xs={10}>
+                <div
+                  style={{
+                    backgroundColor: "#5866d3",
+                    // textAlign: "right",
+                    alignItems: "center",
+                    height: 50,
+                    display: "flex",
+                  }}
+                >
+                  <img
+                    style={{ height: "70%", marginLeft: "72%" }}
+                    src={firebase.auth().currentUser.photoURL}
+                    alt="User Profile"
+                  />
+                  <div style={{ color: "white", marginLeft: 10 }}>
+                    Hey <b>{firebase.auth().currentUser.displayName}</b>
+                  </div>
+                </div>
+                <Switch>
+                  <Route exact path="/">
+                    <Home token={token} />
+                  </Route>
+                  <Route exact path="/onboard" component={UserOnBoard} />
+                  <Route exact path="/league/:leagueID" component={League} />
+                  <Route
+                    exact
+                    path="/league/:leagueID/trade/:stock"
+                    component={LeagueTrade}
+                  />
+                </Switch>
+              </Grid>
+            </Grid>
           </Router>
         </div>
       );
