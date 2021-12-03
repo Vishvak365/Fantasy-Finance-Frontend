1a579f005bf1febf083103cd3a07c14a5860a679
Ashish Goolla <ashish.goolla@gmail.com>
2021-12-03 18:38:50 -0500

    update premium page

--- src/pages/Premium.js
+++ src/pages/Premium.js
@@ -2,8 +2,6 @@ import React from "react";
 import "firebase/auth";
 import { Button, Grid, Paper, Autocomplete, TextField } from "@mui/material";
 export default function Premium(props) {
-
-
   const HeaderStyle = {
     borderRadius: "25px",
     padding: 0,
@@ -26,7 +24,7 @@ export default function Premium(props) {
 
   const PremiumStyle = {
     borderRadius: "35px",
-    padding:10,
+    padding: 10,
     height: "5vh",
     width: 175,
     margin: "35px auto",
@@ -36,45 +34,28 @@ export default function Premium(props) {
     <div>
       <Grid>
         <Paper style={HeaderStyle}>
-      <h1>Premium</h1>
-      </Paper>
-      <Paper elevation={10} style={AboutStyle}>
-      <p>This is the premium page. For the one time fee of $12, you will have access to join and create as many Leagues as you want with your friends.</p>
-      <form
-        action={`https://fantasy-finance-backend.herokuapp.com/checkout?token=${props.token}`}
-        // action={`http://localhost:8080/checkout?token=${props.token}`}
-        method="POST"
-      >
-        <Paper elevation={12} style={PremiumStyle}>
-        <Button type="submit" role="link">
-          Purchase Premium
-        </Button>
+          <h1>Premium</h1>
         </Paper>
-      </form>
-      </Paper>
+        <Paper elevation={10} style={AboutStyle}>
+          <p>
+            This is the premium page. For the one time fee of $12, you will have
+            access to join and create as many Leagues as you want with your
+            friends.
+          </p>
+          <form
+            action={`https://fantasy-finance-backend.herokuapp.com/checkout?token=${props.token}`}
+            // action={`http://localhost:8080/checkout?token=${props.token}`}
+            method="POST"
+          >
+            <Paper elevation={12} style={PremiumStyle}>
+              <Button type="submit" role="link">
+                Purchase Premium
+              </Button>
+            </Paper>
+          </form>
+        </Paper>
       </Grid>
     </div>
   );
 }
 
-
-
-{/* <Paper elevation={10} style={AboutStyle}>
-          <h1>Welcome to Fantasy Finance</h1>
-          <Grid align="center"></Grid>
-          <p>Fantasy Finance is a competitive paper trading application. Fantasy Finance allows you to join fantasy leagues where you can compete against your friends or other users by buying and selling stocks. This platform utilizes real financial market data to allow you to build your paper portfolio and compete inorder to create the best return on investment.</p>
-          <Paper elevation={10} style={PremiumStyle}>
-          <Button onClick={() => history.push("/")}>Start</Button>
-          </Paper>
-          <form
-          action={`https://fantasy-finance-backend.herokuapp.com/checkout?token=${props.token}`}
-          method="POST"
-          >
-          <p>You can join the preimum account for $12</p>
-          <Paper elevation={10} style={PremiumStyle}>
-          <Button type="submit" role="link">
-            Join Premium
-          </Button>
-          </Paper>
-          </form>
-        </Paper> */}
